5232d1679ae7b38778585a3a1499b76611d48555
KANGHANNARA <62852426+KANGHANNARA@users.noreply.github.com>
2023-06-26 15:28:50 +0900

    [unrated] Title: 수 조작하기 2, Time: 18.33 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,7 @@
+def solution(numLog):
+    answer = ''
+    dic = { 1: "w", -1: "s", 10: "d", -10: "a" }
+    for i , v in enumerate(numLog):
+        if i != len(numLog)-1:
+            answer += dic[numLog[i+1] - numLog[i]]
+    return answer
