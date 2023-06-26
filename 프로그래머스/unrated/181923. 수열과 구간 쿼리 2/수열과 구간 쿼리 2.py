9fa504ba7d326015480daa5c871987090550a2cc
KANGHANNARA <62852426+KANGHANNARA@users.noreply.github.com>
2023-06-26 16:36:43 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 9.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,14 +1,9 @@
 def solution(arr, queries):
     answer = []
-    a = []
-    for s,e,k in queries:
-        if e == len(arr):
-            temp = [x for x in arr[s:]]
-        else:
-            temp = [x for x in arr[s:e+1]]
-        condi = sorted([x for x in temp if x > k])
-        if len(condi) == 0:
-            answer.append(-1)
-        else:
-            answer.append(condi[0])   
+    for s, e, k in queries:
+        a = []
+        for val in arr[s:e+1]:
+            if val > k:
+                a.append(val)
+        answer.append(-1 if not a else min(a))
     return answer
