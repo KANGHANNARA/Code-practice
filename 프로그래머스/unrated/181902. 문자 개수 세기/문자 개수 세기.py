48e4c518559db0e998d67ff443e573070d3a1223
KANGHANNARA <62852426+KANGHANNARA@users.noreply.github.com>
2023-07-04 11:38:54 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,7 +1,7 @@
 from string import ascii_lowercase
 def solution(my_string):
     alphabet_list = list(ascii_lowercase)
-    upper_list = [i.capitalize() for i in alphabet_list]
+    upper_list = [i.upper() for i in alphabet_list]
     answer = []
     for i in upper_list:
         answer.append(my_string.count(i))
