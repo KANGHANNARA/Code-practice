5b57522ee1b2856c499cc3684ade1d3bcc3ce044
KANGHANNARA <62852426+KANGHANNARA@users.noreply.github.com>
2023-07-21 12:12:09 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.49 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -2,11 +2,11 @@ def solution(s):
     answer = []
     s_dict = dict()
     for idx,val in enumerate(s):
-        if val not in s_dict:
+        if val not in s_dict: # s_dict에 없는 값은 answer에 -1 
             answer.append(-1)
-            s_dict[val] = idx
+            s_dict[val] = idx # val값에 인덱스 값 수정
         else:
-            num = idx - s_dict[val]
+            num = idx - s_dict[val] # 인덱스값에서 s_dict에 있는 인덱스 값 빼기(거리 계산)
             answer.append(num)
-            s_dict[val] =idx
-    return answer
+            s_dict[val] =idx # val값에 인덱스 값 수정
+    return  answer
