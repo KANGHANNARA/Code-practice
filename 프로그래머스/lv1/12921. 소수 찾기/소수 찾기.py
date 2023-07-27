b90822d9f2df6ed01edc6ffa8dca26503a1da42d
KANGHANNARA <62852426+KANGHANNARA@users.noreply.github.com>
2023-07-27 13:48:38 +0900

    [level 1] Title: 소수 찾기, Time: 326.79 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,4 +1,4 @@
-import math
+'''import math
 from itertools import *
 def is_number(s):
     for i in range(2, int(math.sqrt(s) + 1)):
@@ -11,4 +11,11 @@ def solution(n):
     for i in range(2, n+1):
         if is_number(i) == True:
             answer += 1
-    return answer
+    return answer'''
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
