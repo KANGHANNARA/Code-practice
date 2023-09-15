d56a9b981a7a7007db34744407f61ac3f4edf8dd
KANGHANNARA <62852426+KANGHANNARA@users.noreply.github.com>
2023-09-15 21:20:12 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = 0
-    for a in range(n+1,1000000):
+    for a in range(n+1,1000001):
         if bin(n).count('1') == bin(a).count('1'):
             answer = a
             break
